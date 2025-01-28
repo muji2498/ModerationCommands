19cce1fe4cda1ca23ca62403e8fd5deb7a33e1b8
Muj <muji2498@users.noreply.github.com>
2025-01-28 00:05:12 +0000

    Log every 50 incidents as to not bog down console Now track multiple units for one player Fix out of bounds error for commands

--- Commands/FriendlyFireCommands.cs
+++ Commands/FriendlyFireCommands.cs
@@ -9,79 +9,89 @@ public class FriendlyFireCommands
     [ConsoleCommand("friendlyfire", Roles.Owner | Roles.Admin)]
     public static void FriendlyFire(string[] args, CommandObjects context)
     {
+        if (args.Length < 1)
+        {
+            var currentValue = ModerationPlugin.Config.Enabled.Value ? "Enabled" : "Disabled";
+            Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Usage: friendlyfire true|false. Current Value: {currentValue}", context.Player, false);
+            return;
+        }
+        
         if (bool.TryParse(args[0], out bool friendlyFireToggle))
         {
             ModerationPlugin.Config.Enabled.Value = friendlyFireToggle;
             var what = friendlyFireToggle ? "Enabled" : "Disabled";
             Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Friendly-fire: {what}", context.Player, false);
         }
-        else
-        {
-            var currentValue = ModerationPlugin.Config.Enabled.Value ? "Enabled" : "Disabled";
-            Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Usage: friendlyfire true|false. Current Value: {currentValue}", context.Player, false);
-        }
     }
 
     [ConsoleCommand("friendlyfire.kickonkill", Roles.Owner | Roles.Admin)]
     public static void FriendlyFireOnKill(string[] args, CommandObjects context)
     {
+        if (args.Length < 1)
+        {
+            var currentValue = ModerationPlugin.Config.KickOnKill.Value ? "Enabled" : "Disabled";
+            Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Usage: friendlyfire.kickonkill true|false. Current Value: {currentValue}", context.Player, false);
+            return;
+        }
+        
         if (bool.TryParse(args[0], out bool friendlyFireToggle))
         {
             ModerationPlugin.Config.KickOnKill.Value = friendlyFireToggle;
             var what = friendlyFireToggle ? "Enabled" : "Disabled";
             Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Friendly-fire kick on kill: {what}", context.Player, false);
         }
-        else
-        {
-            var currentValue = ModerationPlugin.Config.KickOnKill.Value ? "Enabled" : "Disabled";
-            Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Usage: friendlyfire.kickonkill true|false. Current Value: {currentValue}", context.Player, false);
-        }
     }
 
     [ConsoleCommand("friendlyfire.player", Roles.Owner | Roles.Admin)]
     public static void FriendlyFirePlayer(string[] args, CommandObjects context)
     {
+        if (args.Length < 1)
+        {
+            var currentValue = ModerationPlugin.Config.FriendlyFirePlayerKick.Value ? "Enabled" : "Disabled";
+            Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Usage: friendlyfire.player true|false. Current Value: {currentValue}", context.Player, false);
+            return;
+        }
+        
         if (bool.TryParse(args[0], out bool toggle))
         {
             ModerationPlugin.Config.FriendlyFirePlayerKick.Value = toggle;
             var what = toggle ? "Enabled" : "Disabled";
             Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Player friendly-fire: {what}", context.Player, false);
         }
-        else
-        {
-            var currentValue = ModerationPlugin.Config.FriendlyFirePlayerKick.Value ? "Enabled" : "Disabled";
-            Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Usage: friendlyfire.player true|false. Current Value: {currentValue}", context.Player, false);
-        }
     }
 
     [ConsoleCommand("friendlyfire.unit", Roles.Owner | Roles.Admin)]
     public static void FriendlyFireUnit(string[] args, CommandObjects context)
     {
+        if (args.Length < 1)
+        {
+            var currentValue = ModerationPlugin.Config.FriendlyFireUnitKick.Value ? "Enabled" : "Disabled";
+            Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Usage: friendlyfire.unit true|false. Current Value: {currentValue}", context.Player, false);
+            return;
+        }
+        
         if (bool.TryParse(args[0], out bool toggle))
         {
             ModerationPlugin.Config.FriendlyFireUnitKick.Value = toggle;
             var what = toggle ? "Enabled" : "Disabled";
             Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Unit friendly-fire: {what}", context.Player, false);
         }
-        else
-        {
-            var currentValue = ModerationPlugin.Config.FriendlyFireUnitKick.Value ? "Enabled" : "Disabled";
-            Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Usage: friendlyfire.unit true|false. Current Value: {currentValue}", context.Player, false);
-        }
     }
 
     [ConsoleCommand("friendlyfire.maxincidents", Roles.Owner | Roles.Admin)]
     public static void FriendlyFireMaxIncidents(string[] args, CommandObjects context)
     {
+        if (args.Length < 1)
+        {
+            var currentValue = ModerationPlugin.Config.FriendlyFireMaxIncidents.Value;
+            Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Usage: friendlyfire.maxincidents amount. Current Value: {currentValue}", context.Player, false);
+            return;
+        }
+        
         if (int.TryParse(args[0], out int amount))
         {
             ModerationPlugin.Config.FriendlyFireMaxIncidents.Value = amount;
             Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Friendly-fire max incidents set to {amount}", context.Player, false);
-        }
-        else
-        {
-            var currentValue = ModerationPlugin.Config.FriendlyFireMaxIncidents.Value;
-            Wrapper.ChatManager.TargetReceiveMessage(context.Player.Owner, $"Usage: friendlyfire.maxincidents amount. Current Value: {currentValue}", context.Player, false);
         }
     }
 
--- Data/IncidentData.cs
+++ Data/IncidentData.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Moderation.Data;
 
 public class IncidentData
 {
     public Player Player { get; set; }
-    public Unit DamagedUnit { get; set; }
+    public Dictionary<Unit, UnitIncident> UnitIncidents { get; set; } = new();
+    public DateTime LastReported { get; set; }
+}
+
+public class UnitIncident
+{
     public int KillCount { get; set; }
     public int DamageCount { get; set; }
-    public DateTime LastReported { get; set; }
+
+    public bool HasBeenReported { get; set; } = false;
 }
